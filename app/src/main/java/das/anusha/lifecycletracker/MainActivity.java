0e655a5948403f285aef457759ddf079373d83c1
Raccoon15834 <anusha.das4fx@gmail.com>
2021-09-20 23:29:14 -0400

    set layout and ids

--- app/src/main/java/das/anusha/lifecycletracker/MainActivity.java
+++ app/src/main/java/das/anusha/lifecycletracker/MainActivity.java
@@ -2,17 +2,30 @@ package das.anusha.lifecycletracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    SharedPreferences.Editor myNums;
+    String prefTag = "das.anusha.lifecycleTracker.myNums";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences myNumSaves = getSharedPreferences(prefTag, Context.MODE_PRIVATE);
+        myNums = myNumSaves.edit(); //make sure to apply putINts
+
+
     }
 }
 
 //P3L05aDasAnushaLifecycle.apk
 //Video, main activity java
 //embellish- new apk and video
+
+//reset
+//Create and Use a Custom Class with an appropriate name to store your variables (use alt+insert to generate getters and setters)
+//Converting lists of objects
+//Using GSON/JSON to convert object to String
+
